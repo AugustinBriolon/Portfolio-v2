49807aa199f995a03f34bfbfb1d5616c120c69d5
Augustin <augustin.briolon@gmail.com>
2022-11-07 10:10:13 +0100

    :mag: seo improve

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,19 @@
 import Nav from '../components/Nav/Nav.js';
 import Error from '../components/Error/Error.js';
+import MetaTags from 'react-meta-tags';
 
 
 const NotFound = () =>
   <div>
 
+    <MetaTags>
+      <meta name="robots" content="noindex" />
+      <meta name="googlebot" content="noindex" />
+    </MetaTags>
+
     <Nav />
     <Error />
-  
+
   </div>
 
 export default NotFound
